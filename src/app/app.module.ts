8c5057f4cc65b92ba7f75fff91982e40519944ff
CharlyAlbert0 <alberto.carlos.esco@gmail.com>
2019-06-17 08:33:23 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+
+import {ElasticsearchService} from './elasticsearch.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [ElasticsearchService],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
---
+++ src/app/elasticsearch.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+import { Client } from 'elasticsearch';
+
+@Injectable()
+export class ElasticsearchService {
+
+  private client: Client;
+
+  constructor() {
+    if (!this.client) {
+      this.connect();
+    }
+  }
+
+  private connect() {
+    this.client = new Client({
+      host: 'http://localhost:9200',
+      log: 'trace'
+    });
+  }
+
+  createIndex(name): any {
+    return this.client.indices.create(name);
+  }
+
+  isAvailable(): any {
+    return this.client.ping({
+      requestTimeout: Infinity,
+      body: 'hello JavaSampleApproach!'
+    });
+  }
+}
